8520ad481f9e81dd4b5b68b8a3fdffb3f1491da0
Alan Velasques Santos <alanvelasques@gmail.com>
2017-12-27 16:35:54 -0200

    Histórias paralelas até o desafio 1 agora completo

---
+++ maissolidos2.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <title>OA</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="./js/bootstrap.min.js"></script>
+  </head>
+  <body>
+      <div class="container-fluid" style="min-height: 100%; min-height: 100vh; display: flex; align-items: center;">
+        <div class="row vertical-align" style="display: flex; align-items: flex-end;">
+          <div class="col align-bottom">
+            <div class="card" style="text-align:right; border:none;">
+              <a href="maissolidos.php"><img class="card-img-top" src="./img/COMANDOS/01SETAVOLTAAZUL.png" alt="Voltar" style="width:40%;"></a>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="card" style="border:none;">
+              <img class="card-img-top" src="./img/LINKS/SOLIDOSAVANCADOS2.jpg" alt="Sólidos avançados">
+            </div>
+          </div>
+          <div class="col align-bottom">
+            <div class="card" style="border:none;">
+              <a href="09.php"><img class="card-img-top" src="./img/COMANDOS/01SETAAVANCAVERM.png" alt="Avançar" style="width:40%;"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+  </body>
+</html>
