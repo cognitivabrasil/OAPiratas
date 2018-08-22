d491af96dec1d882fb5d3e389d231e70f2909664
Alan Velasques Santos <alanvelasques@gmail.com>
2018-08-22 18:32:08 -0300

    Questionarios finais

--- final.php
+++ final.php
@@ -14,55 +14,79 @@
           </div>
           <div class="col-6">
             <div class="card" style="border:none;">
-                <img class="card-img-top" src="./img/HISTORIA/019.jpg" alt="019">
+                <img class="card-img-top" src="./img/HISTORIA/019.jpg" alt="019" style="height: 1px;">
                 <p>Perfil</p>
                 <form action="sendEmail.php" method="post">
-                    <input type="text" name="nascimento" placeholder="Data de nascimento no formato dd/mm/aaaa">
+                    <input type="text" name="nascimento" placeholder="Data de nascimento no formato dd/mm/aaaa"><br>
+                    <label for="sexo">Sexo:</label>
                     <select name="sexo">
                         <option value="masculino">Masculino</option>
                         <option value="feminino">Feminino</option>
                         <option value="outros">Outros</option>
-                    </select>
+                    </select><br>
+                    <label for="surdez">Possui algum grau de surdez? </label>
                     <select name="surdez">
                         <option value="nao">Não</option>
                         <option value="sim">Sim</option>
-                    </select>
+                    </select><br>
+                    <label for="surdez">Nível de surdez:</label>
                     <select name="nivel_surdez">
                         <option value="profundo">Profundo</option>
                         <option value="parcial">Parcialmente surdo</option>
                         <option value="outros">Outros</option>
-                    </select>
+                    </select><br>
+                    <label for="origem_surdez">Quando ocorreu a surdez?</label>
                     <select name="origem_surdez">
                         <option value="natisurdo">Natisurdo</option>
                         <option value="antes">Antes de aprender a língua portuguesa oral/escrita</option>
                         <option value="depois">Depois de aprender a língua portuguesa oral/escrita</option>
-                    </select>
+                    </select><br>
+                    <label for="lingua">Qual sua primeira língua?</label>
                     <select name="lingua">
                         <option value="portuguesa">Língua portuguesa oral/escrita</option>
                         <option value="libras">LIBRAS</option>
-                    </select>
+                    </select><br>
+                    <label for="visual">Possui alguma deficiência visual?</label>
                     <select name="visual">
                         <option value="nao">Não</option>
                         <option value="sim">Sim</option>
-                    </select>
+                    </select><br>
+                    <label for="nivel_visual">Nível da deficiência visual:</label>
                     <select name="nivel_visual">
                         <option value="cego">Cego</option>
                         <option value="parcialmente">Enxerga parcialmente</option>
                         <option value="outro">Outro</option>
-                    </select>
+                    </select><br>
+                    <label for="origem_visual">Quando ocorreu a deficiência visual?</label>
                     <select name="origem_visual">
                         <option value="nascimento">Nascimento</option>
                         <option value="infancia">Infância</option>
                         <option value="adolescencia">Adolescência</option>
                         <option value="adulto">Adulto</option>
-                    </select>
+                    </select><br>
+                    <label for="comunicacao">Qual sua forma natural de comunicação?</label>
                     <select name="comunicacao">
                         <option value="fala">Fala</option>
                         <option value="escrita">Escrita</option>
                         <option value="libras">LIBRAS</option>
                         <option value="outra">Outra</option>
-                    </select>
-                    <input type="text" name="nascimento" placeholder="Data de nascimento no formato dd/mm/aaaa">
+                    </select><br>
+                    <label for="quadrinhos">Qual sua opinião sobre histórias em quadrinhos?</label>
+                    <select name="quadrinhos">
+                        <option value="gosto">Gosto</option>
+                        <option value="ngosto">Não Gosto</option>
+                        <option value="outra">Outra</option>
+                    </select><br>
+                    <label for="porque_quadrinhos">Porque? </label><input type="text" name="porque_quadrinhos"><br>
+                    <label for="preferido">Cite qual história em quadrinhos preferido: </label><input type="text" name="preferido"><br>
+                    <label for="quadrinhos">Qual sua opinião sobre jogos?</label>
+                    <select name="quadrinhos">
+                        <option value="gosto">Gosto</option>
+                        <option value="ngosto">Não Gosto</option>
+                        <option value="outra">Outra</option>
+                    </select><br>
+                    <label for="porque_jogos">Porque? </label><input type="text" name="porque_jogos"><br>
+                    <label for="preferido">Cite qual jogo preferido: </label><input type="text" name="preferido" placeholder="dd/mm/aaaa"><br>
                     <input type="submit" value="enviar">
                 </form>
             </div>
--- sendEmail.php
+++ sendEmail.php
@@ -2,12 +2,13 @@
 $email_to = 'contato@lamid.egc.ufsc.br';
 $email_from = 'contato@lamid.egc.ufsc.br';
 $subject = 'Objeto de aprendizagem - Piratas';
-$message = 'Uso do Aluno x';
+$message = 'Questionários respondidos:';
+$message .= $_POST['texto'];
 $headers = 'From: '.$email_from."\r\n" .
     'Reply-To: '.$email_from."\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-echo $headers;
+echo $message;
 //mail($email_to, $subject, $message, $headers);
 
 ?>
